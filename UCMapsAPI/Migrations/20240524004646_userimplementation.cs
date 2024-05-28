acec90fc2105284cd0b6892b9c7e086858ceb8d5
JaredWalden00 <jaredwalden00@gmail.com>
2024-05-27 21:00:16 -0400

    migrations

---
+++ UCMapsAPI/Migrations/20240524004646_userimplementation.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UCMapsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class userimplementation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Marker",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TokenInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TokenInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Marker_UserId",
+                table: "Marker",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Marker_Users_UserId",
+                table: "Marker",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Marker_Users_UserId",
+                table: "Marker");
+
+            migrationBuilder.DropTable(
+                name: "TokenInfo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Marker_UserId",
+                table: "Marker");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Marker");
+        }
+    }
+}
